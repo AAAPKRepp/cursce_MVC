7b9a0adc1d810ca7e2f97f2455c94f8566584ead
Missk-cell <twiihc@gmail.com>
2025-02-03 16:43:49 +0300

    Добавьте файлы проекта.

---
+++ cursce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cursce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
